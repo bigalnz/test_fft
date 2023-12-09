3795d67ae2f54f785fe2cc3087e1f5fe7fb745e7
nocarryr <matt@nomadic-recording.com>
2023-12-09 13:43:31 -0600

    Move sample processing into its own class

--- test_fft.py
+++ test_fft.py
@@ -13,11 +13,10 @@ if TYPE_CHECKING:
     from rtlsdr.rtlsdr import RtlSdr
 
 
+SamplesT = npt.NDArray[np.complex128]
+"""Alias for sample arrays"""
 
-# Number of samples to read in each iteration
-#samples_to_process = 4.8e6
-threshold = 0.9
-freq_offset = -43.8e4 #Hz
+
 
 class SampleReader:
     sample_rate: float = 2.4e6
@@ -41,7 +40,7 @@ class SampleReader:
             raise RuntimeError('SampleReader not open')
         return [v / 10 for v in self.sdr.gain_values]
 
-    def read_samples(self) -> npt.NDArray[np.complex128]:
+    def read_samples(self) -> SamplesT:
         """Read :attr:`num_samples` from the device
         """
         if self.sdr is None:
@@ -97,80 +96,93 @@ class SampleReader:
         self.close()
 
 
-stateful_index = 0
+class SampleProcessor:
+    threshold: float = 0.9
+    freq_offset: float = -43.8e4 #Hz
+    beep_duration: float = 0.017 # seconds
 
-def process_samples(samples, sample_rate, freq_offset, threshold):
+    sample_rate: float
+    stateful_index: int
 
-    global stateful_index
+    def __init__(self, sample_rate: float) -> None:
+        self.sample_rate = sample_rate
+        self.stateful_index = 0
 
-    beep_duration = 0.017 # seconds
-    fft_size = int(beep_duration * sample_rate / 2) # this makes sure there's at least 1 full chunk within each beep
-    f = np.linspace(sample_rate/-2, sample_rate/2, fft_size)
-    num_ffts = len(samples) // fft_size # // is an integer division which rounds down
-    fft_thresh = 0.1
-    beep_freqs = []
-    for i in range(num_ffts):
-        fft = np.abs(np.fft.fftshift(np.fft.fft(samples[i*fft_size:(i+1)*fft_size]))) / fft_size
-        if np.max(fft) > fft_thresh:
-            beep_freqs.append(np.linspace(sample_rate/-2, sample_rate/2, fft_size)[np.argmax(fft)])
-        plt.plot(f,fft)
-    #print(beep_freqs)
-    #plt.show()
+    @property
+    def fft_size(self) -> int:
+        # this makes sure there's at least 1 full chunk within each beep
+        return int(self.beep_duration * self.sample_rate / 2)
 
-    t = np.arange(len(samples))/sample_rate
-    samples = samples * np.exp(2j*np.pi*t*freq_offset)
-    h = signal.firwin(501, 0.02, pass_zero=True)
-    samples = np.convolve(samples, h, 'valid')
-    samples = samples[::100]
-    sample_rate = sample_rate/100
-    samples = np.abs(samples)
-    samples = np.convolve(samples, [1]*10, 'valid')/10
-    max_samp = np.max(samples)
-    # samples /= np.max(samples)
-    #print(f"max sample : {max_samp}")
-    #plt.plot(samples)
-    #plt.show()
+    def process(self, samples: SamplesT):
+        fft_size = self.fft_size
+        f = np.linspace(self.sample_rate/-2, self.sample_rate/2, fft_size)
+        num_ffts = len(samples) // fft_size # // is an integer division which rounds down
+        fft_thresh = 0.1
+        beep_freqs = []
+        for i in range(num_ffts):
+            fft = np.abs(np.fft.fftshift(np.fft.fft(samples[i*fft_size:(i+1)*fft_size]))) / fft_size
+            if np.max(fft) > fft_thresh:
+                beep_freqs.append(np.linspace(self.sample_rate/-2, self.sample_rate/2, fft_size)[np.argmax(fft)])
+            plt.plot(f,fft)
+        #print(beep_freqs)
+        #plt.show()
 
-    # Get a boolean array for all samples higher or lower than the threshold
-    low_samples = samples < threshold
-    high_samples = samples >= threshold
+        t = np.arange(len(samples))/self.sample_rate
+        samples = samples * np.exp(2j*np.pi*t*self.freq_offset)
+        h = signal.firwin(501, 0.02, pass_zero=True)
+        samples = np.convolve(samples, h, 'valid')
+        samples = samples[::100]
+        sample_rate = self.sample_rate/100
+        samples = np.abs(samples)
+        samples = np.convolve(samples, [1]*10, 'valid')/10
+        max_samp = np.max(samples)
+        # samples /= np.max(samples)
+        #print(f"max sample : {max_samp}")
+        #plt.plot(samples)
+        #plt.show()
 
-    # Compute the rising edge and falling edges by comparing the current value to the next with
-    # the boolean operator & (if both are true the result is true) and converting this to an index
-    # into the current array
-    rising_edge_idx = np.nonzero(low_samples[:-1] & np.roll(high_samples, -1)[:-1])[0]
-    falling_edge_idx = np.nonzero(high_samples[:-1] & np.roll(low_samples, -1)[:-1])[0]
+        # Get a boolean array for all samples higher or lower than the threshold
+        low_samples = samples < self.threshold
+        high_samples = samples >= self.threshold
 
-    # This would need to be handled more gracefully with a stateful
-    # processing (e.g. saving samples at the end if the pulse is in-between two processing blocks)
-    # Remove stray falling edge at the start
-    if len(rising_edge_idx) == 0 or len(falling_edge_idx) == 0:
-        return
-    #print(f"passed len test for idx's")
-    if rising_edge_idx[0] > falling_edge_idx[0]:
-        falling_edge_idx = falling_edge_idx[1:]
+        # Compute the rising edge and falling edges by comparing the current value to the next with
+        # the boolean operator & (if both are true the result is true) and converting this to an index
+        # into the current array
+        rising_edge_idx = np.nonzero(low_samples[:-1] & np.roll(high_samples, -1)[:-1])[0]
+        falling_edge_idx = np.nonzero(high_samples[:-1] & np.roll(low_samples, -1)[:-1])[0]
 
-    # Remove stray rising edge at the end
-    if rising_edge_idx[-1] > falling_edge_idx[-1]:
-        rising_edge_idx = rising_edge_idx[:-1]
+        # This would need to be handled more gracefully with a stateful
+        # processing (e.g. saving samples at the end if the pulse is in-between two processing blocks)
+        # Remove stray falling edge at the start
+        if len(rising_edge_idx) == 0 or len(falling_edge_idx) == 0:
+            return
+        #print(f"passed len test for idx's")
+        if rising_edge_idx[0] > falling_edge_idx[0]:
+            falling_edge_idx = falling_edge_idx[1:]
 
-    rising_edge_diff = np.diff(rising_edge_idx)
-    time_between_rising_edge = sample_rate / rising_edge_diff * 60
+        # Remove stray rising edge at the end
+        if rising_edge_idx[-1] > falling_edge_idx[-1]:
+            rising_edge_idx = rising_edge_idx[:-1]
 
-    pulse_widths = falling_edge_idx - rising_edge_idx
-    rssi_idxs = list(np.arange(r, r + p) for r, p in zip(rising_edge_idx, pulse_widths))
-    rssi = [np.mean(samples[r]) * max_samp for r in rssi_idxs]
+        rising_edge_diff = np.diff(rising_edge_idx)
+        time_between_rising_edge = sample_rate / rising_edge_diff * 60
 
-    for t, r in zip(time_between_rising_edge, rssi):
-        print(f"BPM: {t:.02f}")
-        print(f"rssi: {r:.02f}")
-    stateful_index += len(samples)
-    print(f"stateful index : {stateful_index}")
+        pulse_widths = falling_edge_idx - rising_edge_idx
+        rssi_idxs = list(np.arange(r, r + p) for r, p in zip(rising_edge_idx, pulse_widths))
+        rssi = [np.mean(samples[r]) * max_samp for r in rssi_idxs]
+
+        for t, r in zip(time_between_rising_edge, rssi):
+            print(f"BPM: {t:.02f}")
+            print(f"rssi: {r:.02f}")
+        self.stateful_index += len(samples)
+        print(f"stateful index : {self.stateful_index}")
+
 
 
 # NOTE: Always better to run things within a main function
 def main():
     with SampleReader() as reader:
+        processor = SampleProcessor(reader.sample_rate)
         try:
             while True:
                 # Read a chunk of samples
@@ -187,13 +199,7 @@ def main():
                 #show()
 
                 # Process the samples
-
-                processed_data = process_samples(
-                    samples,
-                    reader.sample_rate,
-                    freq_offset,
-                    threshold,
-                )
+                processor.process(samples)
 
         except KeyboardInterrupt:
             # Stop the loop on keyboard interrupt
