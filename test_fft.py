b8c061efff8b7537f2345477f74c06db4e2cf2f8
nocarryr <matt@nomadic-recording.com>
2023-12-08 18:05:14 -0600

    Add SampleReader class to handle sdr, sample collection

--- test_fft.py
+++ test_fft.py
@@ -1,21 +1,88 @@
+from typing import Self, TYPE_CHECKING
 import numpy as np
+import numpy.typing as npt
 import matplotlib.pyplot as plt
 from rtlsdr import *
 from pylab import *
 import time
 from scipy import signal
 
-# Configure RTL-SDR parameters
-sdr = RtlSdr()
-sdr.sample_rate = 2.4e6  # 2.4 MHz
-sdr.center_freq = 160270968 # MHz
-sdr.gain = 445
+if TYPE_CHECKING:
+    # NOTE: This is just for static type checking because rtlsdr.RtlSdr isn't
+    #       really set up properly.. I should probably fix that
+    from rtlsdr.rtlsdr import RtlSdr
+
+
 
 # Number of samples to read in each iteration
 #samples_to_process = 4.8e6
-samples_to_process = 1.024e6
 threshold = 0.9
 freq_offset = -43.8e4 #Hz
+
+class SampleReader:
+    sample_rate: float = 2.4e6
+    center_freq: float = 160270968
+
+    gain: float|str = 44.5
+    """gain in dB"""
+
+    num_samples: int = int(1.024e6)
+    """Number of samples to read in each iteration"""
+
+    sdr: RtlSdr|None = None
+    """RtlSdr instance"""
+
+    def read_samples(self) -> npt.NDArray[np.complex128]:
+        """Read :attr:`num_samples` from the device
+        """
+        if self.sdr is None:
+            raise RuntimeError('SampleReader not open')
+        samples = self.sdr.read_samples(self.num_samples)
+        if TYPE_CHECKING:
+            # This is just because `read_samples()`` can return a list
+            # if numpy isn't installed
+            assert isinstance(samples, np.ndarray)
+        return samples
+
+    def open(self):
+        """Open the device and set all necessary parameters
+        """
+        assert self.sdr is None
+        if TYPE_CHECKING:
+            # NOTE: Another workaround for the above TYPE_CHECKING stuff
+            #       (just ignore for now)
+            assert RtlSdr is not None
+        sdr = self.sdr = RtlSdr()
+        sdr.sample_rate = self.sample_rate
+        sdr.center_freq = self.center_freq
+        sdr.gain = self.gain
+
+        # Now we should read the *actual* values back from the device
+        self.sample_rate = sdr.sample_rate
+        self.center_freq = sdr.center_freq
+        self.gain = sdr.gain
+
+    def close(self):
+        """Close the device if it's currently open
+        """
+        sdr = self.sdr
+        if sdr is None:
+            return
+        self.sdr = None
+        sdr.close()
+
+    # NOTE: These two methods allow use as a context manager:
+    #       with SampleReader() as reader:
+    #           ...
+    #
+    #       This way it will always close when the program does
+    def __enter__(self) -> Self:
+        self.open()
+        return self
+
+    def __exit__(self, *args):
+        self.close()
+
 
 stateful_index = 0
 
@@ -50,7 +117,7 @@ def process_samples(samples, sample_rate, freq_offset, threshold):
     #print(f"max sample : {max_samp}")
     #plt.plot(samples)
     #plt.show()
-    
+
     # Get a boolean array for all samples higher or lower than the threshold
     low_samples = samples < threshold
     high_samples = samples >= threshold
@@ -87,29 +154,41 @@ def process_samples(samples, sample_rate, freq_offset, threshold):
     stateful_index += len(samples)
     print(f"stateful index : {stateful_index}")
 
-try:
-    while True:
-        # Read a chunk of samples
-        # samples_to_process is number of samples to process
-        #start = time.time()
-    
-        samples = sdr.read_samples(samples_to_process)
-        #print(f"finish : {time.time()-start}")
 
-        # use matplotlib to estimate and plot the PSD
-        #psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-        #xlabel('Frequency (MHz)')
-        #ylabel('Relative power (dB)')
-        #show()
+# NOTE: Always better to run things within a main function
+def main():
+    with SampleReader() as reader:
+        try:
+            while True:
+                # Read a chunk of samples
+                # samples_to_process is number of samples to process
+                #start = time.time()
 
-        # Process the samples
+                samples = reader.read_samples()
+                #print(f"finish : {time.time()-start}")
 
-        processed_data = process_samples(samples, sdr.sample_rate, freq_offset, threshold)
+                # use matplotlib to estimate and plot the PSD
+                #psd(samples, NFFT=1024, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+                #xlabel('Frequency (MHz)')
+                #ylabel('Relative power (dB)')
+                #show()
 
-except KeyboardInterrupt:
-    # Stop the loop on keyboard interrupt
-    print("Program terminated.")
+                # Process the samples
 
-finally:
-    # Close the RTL-SDR device
-    sdr.close()
+                processed_data = process_samples(
+                    samples,
+                    reader.sample_rate,
+                    freq_offset,
+                    threshold,
+                )
+
+        except KeyboardInterrupt:
+            # Stop the loop on keyboard interrupt
+            print("Program terminated.")
+            return
+
+
+# NOTE: This only calls main() above ONLY when the script is being executed
+#       This way you can import it without running the while loop
+if __name__ == '__main__':
+    main()
