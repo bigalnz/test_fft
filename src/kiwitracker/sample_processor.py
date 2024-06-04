e9680fe2c9e2b72c5df6e1f2528b3ccee060d40b
Andrej Kesely <andrej.kesely@interway.sk>
2024-06-04 10:33:18 +0200

    Testing RPi

--- src/kiwitracker/sample_processor.py
+++ src/kiwitracker/sample_processor.py
@@ -74,8 +74,8 @@ def decimate_samples(
     """
 
     samples = samples * phasor(pc.num_samples_to_process, pc.sample_rate, pc.freq_offset)[: samples.size]
-    # samples = signal.convolve(samples, fir(), "same")
-    fir()
+
+    samples = signal.convolve(samples, fir(), mode="same", method="direct")
     samples = np.abs(samples[::100])
 
     return samples, pc.sample_rate / 100, samples
